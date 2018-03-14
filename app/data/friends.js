09597bbb8ca28fdc758cb0ec7112e0dfff5e5d40
Andrew Jones <Andrew@AHJones-iMac.local>
2018-03-14 00:30:28 -0600

    resetting commit

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+module.exports = {
+    friends: [{
+            name: 'Santa Claus',
+            photoUrl: 'http://wpmedia.vancouversun.com/2017/11/santa-bill1-jpg.jpg?quality=55&strip=all',
+            answers: [
+                1,
+                2,
+                3,
+                4,
+                5,
+                4,
+                3,
+                2,
+                1,
+                2
+            ]
+        },
+
+        {
+            "name": "Ahmed",
+            "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+            "scores": [
+                5,
+                1,
+                4,
+                4,
+                5,
+                1,
+                2,
+                5,
+                4,
+                1
+            ]
+        }
+    ]
+};
